ba4def1d42305d5c1df4fc3045b3936b637c6e25
Lucas Arcanjo <lucassarcanjo@gmail.com>
2022-09-08 04:17:22 -0300

    fix: refresh token behavior

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
 
 const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
@@ -11,11 +11,12 @@ export const api = axios.create({
   },
 });
 
-const login = async () => {
+const login = async (forceRefresh = false) => {
   try {
-    const tokenLocal = localStorage.getItem("token");
-
-    if (tokenLocal) return tokenLocal;
+    if (!forceRefresh) {
+      const tokenLocal = localStorage.getItem("token");
+      if (tokenLocal) return tokenLocal;
+    }
 
     const tokenFromApi = await axios.post<string>(baseURL + "/login", {
       login: import.meta.env.VITE_DEFAULT_LOGIN,
@@ -41,13 +42,18 @@ api.interceptors.request.use(async (config) => {
   return config;
 });
 
-api.interceptors.response.use(async (config) => {
-  if (!config.headers) return;
+api.interceptors.response.use(
+  (config) => config,
+  async (error: Error | AxiosError) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      if (!error.config.headers) return;
 
-  if (config.headers["x-token-expired"]) {
-    const token = await login();
-    config.headers.Authorization = `Bearer ${token}`;
+      const token = await login(true);
+
+      error.config.headers.Authorization = `Bearer ${token}`;
+      return api.request(error.config);
+    }
+
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
