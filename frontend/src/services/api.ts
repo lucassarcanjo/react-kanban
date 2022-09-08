65cf59cd00d631632755d13f0c41257737631b29
Lucas Arcanjo <lucassarcanjo@gmail.com>
2022-09-08 00:28:21 -0300

    feat: add login

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,53 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+
+const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:5000",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+});
+
+const login = async () => {
+  try {
+    const tokenLocal = localStorage.getItem("token");
+
+    if (tokenLocal) return tokenLocal;
+
+    const tokenFromApi = await axios.post<string>(baseURL + "/login", {
+      login: import.meta.env.VITE_DEFAULT_LOGIN,
+      senha: import.meta.env.VITE_DEFAULT_PASSWORD,
+    });
+
+    localStorage.setItem("token", tokenFromApi.data);
+
+    return tokenFromApi.data;
+  } catch (error) {
+    toast.error("Erro ao fazer login");
+  }
+};
+
+api.interceptors.request.use(async (config) => {
+  if (!config.headers) return;
+
+  if (!config.headers?.Authorization) {
+    const token = await login();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(async (config) => {
+  if (!config.headers) return;
+
+  if (config.headers["x-token-expired"]) {
+    const token = await login();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
